b6aa7ef2a246a42f1bcc0711f26ac5010aee68de
DangCongTien <dctien00@gmail.com>
2020-12-17 00:29:30 +0700

    add file blade in admin page

---
+++ resources/views/admin/elements/list-user.blade.php
@@ -0,0 +1,8 @@
+<div class="page-header zvn-page-header clearfix">
+    <div class="zvn-page-header-title">
+        <h3>Danh sách User</h3>
+    </div>
+    <div class="zvn-add-new pull-right">
+    <a href="/form" class="btn btn-success"><iclass="fa fa-plus-circle"></i> Thêm mới</a>
+    </div>
+</div>
